2e2e3736c3da7c9f5dc0276835796df12c01e587
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-31 19:00:48 +0200

    Signup form, TSX structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import SignUpForm from "./components/SignUp/SignUp";
 
 function App() {
-  return <h1>DishWeek</h1>;
+  return (
+    <>
+      <h1>DishWeek</h1>
+      <SignUpForm />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const SignUpForm = (): JSX.Element => {
+  const emptyFields = {
+    name: "",
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(emptyFields);
+
+  const changeData = (event: { target: { id: any; value: any } }) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <p className="greeting">Hello there, please sign up to continue</p>
+      <form
+        className="signup-form"
+        noValidate
+        autoComplete="off"
+        onSubmit={() => {}}
+      >
+        <label htmlFor="name"> Name </label>
+        <input
+          id="name"
+          value={formData.name}
+          onChange={changeData}
+          placeholder="Enter your name"
+        />
+
+        <label htmlFor="username"> Username </label>
+        <input
+          id="username"
+          value={formData.username}
+          onChange={changeData}
+          placeholder="Enter your username"
+        />
+
+        <label htmlFor="password"> Password </label>
+        <input
+          id="password"
+          type="password"
+          value={formData.password}
+          onChange={changeData}
+          placeholder="Enter your password"
+        />
+        <button
+          className="sign-up"
+          type="submit"
+          disabled={
+            formData.username === "" ||
+            formData.password === "" ||
+            formData.name === ""
+          }
+        >
+          Sign Up
+        </button>
+        <p className="change-form">
+          Don't have an account? <span>Sign In</span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
