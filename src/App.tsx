0344b703513599e5e9c4ea5f8f4ec05af94ba34a
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-01 16:09:51 +0200

    Created LogoComplete component and styled too

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import LogoComplete from "./components/LogoComplete/LogoComplete";
 import SignUpForm from "./components/SignUp/SignUpForm";
 
 function App() {
   return (
     <>
+      <LogoComplete />
       <SignUpForm />
     </>
   );
---
+++ src/components/LogoComplete/SyledLogoComplete.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const StyledLogoComplete = styled.div`
+  background-color: #c6c9bf;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  padding: 20px;
+  margin-bottom: -10px;
+
+  .container-logo {
+    width: 60%;
+    display: flex;
+    flex-direction: row;
+    margin-top: 20%;
+    align-items: flex-end;
+    justify-content: center;
+
+    h1 {
+      margin: 0;
+      padding: 0;
+      color: #1c3b5e;
+      font-size: 60px;
+      letter-spacing: 5px;
+    }
+
+    img {
+      display: flex;
+      width: 100px;
+      height: 100px;
+      margin-left: 20px;
+      transform: translateY(-4px);
+    }
+  }
+
+  p {
+    color: #4242429e;
+    text-align: center;
+    font-weight: 900;
+    letter-spacing: 2px;
+    margin-top: 10px;
+    font-size: 16px;
+    margin-bottom: 40px;
+  }
+`;
