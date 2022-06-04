4b68d3b8936bebc938980e66f3f940a1ab9d7735
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-04 20:29:05 +0200

    Test Dish Component

---
+++ src/components/Dish/Dish.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+import { Dish } from "./Dish";
+
+describe("Given a Dish component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should render two buttons", () => {
+      const data = {
+        id: "123",
+        name: "pep",
+        veggy: true,
+        ingredients: ["pep"],
+        image: "pep",
+        resume: "pep",
+        recipe: "pep",
+        cookingTime: "12h",
+        daysOfWeek: ["lunes"],
+        createdBy: "pep",
+      };
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <Dish dishes={data} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const expectName = screen.getByRole("heading", { level: 3 });
+
+      expect(expectName).toBeInTheDocument();
+    });
+  });
+});
