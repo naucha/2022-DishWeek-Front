34c54524d6f5d821246753bfae8077e006cd65b1
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-05 22:52:07 +0200

    Created deleteThunk

--- src/redux/thunks/dishesThunks.tsx
+++ src/redux/thunks/dishesThunks.tsx
@@ -17,3 +17,20 @@ export const getDishesThunk = () => async (dispatch: AppDispatch) => {
     return error.message;
   }
 };
+
+export const deleteDishesThunk =
+  (id: string) => async (dispatch: AppDispatch) => {
+    const route: string = `${process.env.REACT_APP_API_URL}dishes/${id}`;
+    const token = localStorage.getItem("token");
+    try {
+      const { status } = await axios.get(route, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (status === 200) {
+        dispatch(deleteDishesThunk(id));
+      }
+    } catch (error: any) {
+      return error.message;
+    }
+  };
