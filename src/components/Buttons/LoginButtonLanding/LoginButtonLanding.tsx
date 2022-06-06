6be76462eaf5d104876109e9d341595ccb8212f5
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-07 00:38:40 +0200

    Landing Buttons dimension

--- src/components/Buttons/LoginButtonLanding/LoginButtonLanding.tsx
+++ src/components/Buttons/LoginButtonLanding/LoginButtonLanding.tsx
@@ -4,14 +4,14 @@ const StyledLoginButtonLanding = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 320px;
-  height: 60px;
+  width: 280px;
+  height: 50px;
   cursor: pointer;
   color: white;
   background-color: var(--fc-primary);
   font-family: "Merriweather", sans-serif;
   letter-spacing: 3px;
-  font-size: 22px;
+  font-size: 21px;
   font-weight: bolder;
   border-radius: 50px;
   box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.25);
