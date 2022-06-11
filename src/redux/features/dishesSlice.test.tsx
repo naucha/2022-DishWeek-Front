e060be23cbfdd79cdd5b839ec3319b304fe06c08
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-11 13:33:52 +0200

    Test slice delete and create

--- src/redux/features/dishesSlice.test.tsx
+++ src/redux/features/dishesSlice.test.tsx
@@ -1,6 +1,10 @@
-import { mockDishes } from "../../mocks/mocks";
+import { mockDish, mockDishes } from "../../mocks/mocks";
 import { DishesData } from "../../types/types";
-import dishesSlice, { loadDishesActionCreator } from "./dishesSlice";
+import dishesSlice, {
+  createDishActionCreator,
+  deleteDishActionCreator,
+  loadDishesActionCreator,
+} from "./dishesSlice";
 
 describe("Given a loadDishes reducer", () => {
   describe("When receives a initial state and a loadActionCreator with dishes", () => {
@@ -14,6 +18,49 @@ describe("Given a loadDishes reducer", () => {
       const dishesStatus = dishesSlice(initialState, loadDishes);
 
       expect(dishesStatus).toEqual(expectedLists);
+    });
+  });
+});
+
+describe("Given a deleteDishes reducer", () => {
+  describe("When receives a initial with 2 dishes", () => {
+    test("Then it should return a new state with only one of them", () => {
+      const expectedLength = 1;
+      const initialState = mockDishes;
+
+      const deleteAction = deleteDishActionCreator(mockDishes[1].id);
+      const dishes = dishesSlice(initialState, deleteAction);
+
+      expect(dishes).toHaveLength(expectedLength);
+    });
+  });
+});
+
+describe("Given a createDishes reducer", () => {
+  describe("When receives a newDish if we th state only have one", () => {
+    test("Then it should return a new state with the newDish init", () => {
+      const expectedLength = 2;
+      const initialState = mockDish;
+
+      const newDish = {
+        id: "eqwa",
+        name: "arroz",
+        veggie: "true",
+        ingredients: "cebolla",
+        image: "image",
+        resume: "Poner a hervir...",
+        recipe:
+          "El secreto de estos macarrones es el sofrito. No tengas prisa en realizarlo y te quedará de fábula. Pocha la cebolla lentamente hasta que quede bien caramelizada. Entonces, agrega los pimientos, el ajo, el tomate… y, por último, las setas, previamente",
+        cookingtime: "2h",
+        daysofweek: [],
+        createdby: "Grillo",
+      };
+
+      const createAction = createDishActionCreator(newDish);
+
+      const dishes = dishesSlice(initialState, createAction);
+
+      expect(dishes).toHaveLength(expectedLength);
     });
   });
 });
