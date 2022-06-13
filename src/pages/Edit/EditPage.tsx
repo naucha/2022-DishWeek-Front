57f5261c353314dfb8abf2bc651006562176851a
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-13 23:39:04 +0200

    Add useEffect on EditPage

--- src/pages/Edit/EditPage.tsx
+++ src/pages/Edit/EditPage.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import CreateForm from "../../components/CreateForm/CreateForm";
 import { Header } from "../../components/Header/Header";
 import StyledPage from "../../components/styles/StyledPage";
+import { useAppDispatch } from "../../redux/store/hooks";
+import { getDishThunk } from "../../redux/thunks/dishThunk";
 
 export const EditPage = (): JSX.Element => {
+  const { idDish } = useParams();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (idDish) {
+      dispatch(getDishThunk(idDish));
+    }
+  }, [dispatch, idDish]);
+
   return (
     <StyledPage>
       <Header />
