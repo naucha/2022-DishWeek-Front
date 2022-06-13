0b9ed637f68a389ab74fe26a9d383090f279223a
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-13 12:57:02 +0200

    Add useEffect in homePage to get dishes after some state change

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { DishesList } from "../../components/DishesList/DishesList";
 import { Header } from "../../components/Header/Header";
 import StyledPage from "../../components/styles/StyledPage";
+import { useAppDispatch } from "../../redux/store/hooks";
+import { getDishesThunk } from "../../redux/thunks/dishesThunks";
 
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getDishesThunk());
+  }, [dispatch]);
+
   return (
     <StyledPage>
       <Header />
